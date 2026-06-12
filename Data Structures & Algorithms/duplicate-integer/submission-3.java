bfdcb7ea9ef20273cd62df52f875e505148c760b
Shubhradeep Biswas <shubhradeepbiswas2024@gmail.com>
2026-06-13 00:14:02 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        if(nums.length==0){
+            return false;
+        }
+        for(int i =0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+               }
+            }
+        }
+        return false;
+    }
+}
